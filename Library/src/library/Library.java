735fe42947d5bbb4573b122ea7aa45b13c22e273
PashaPasha <vqvqvq@yandex.ru>
2015-01-25 19:44:31 +0300

    fhfh
    fhfgh

--- Library/src/library/Library.java
+++ Library/src/library/Library.java
@@ -3,7 +3,7 @@ package library;
 public class Library {
 
     public static void main(String[] args) {
-        System.out.println("бла-бла-бла!!!тротротр");
+        System.out.println("бла-бла-бла!!!тротротрсмимтиьтб");
     }
     
 }
